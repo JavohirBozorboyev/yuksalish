513e5fa8de1ea2a6a13c8f2626c9fcb6e5bc6980
UZB02 <muhsinbekmirzamatov3@gmail.com>
2025-03-05 13:25:19 +0500

    sell product modal done

--- src/views/Ombor/MahsulotOmboriView.vue
+++ src/views/Ombor/MahsulotOmboriView.vue
@@ -10,7 +10,7 @@
         </button>
       </RouterLink>
     </div>
-    
+
     <div
       v-if="!data"
       class="w-full h-full bg-white min-h-[60vh] rounded-md flex items-center justify-center mt-5"
@@ -105,6 +105,7 @@
           <hr class="my-4" />
           <div class="grid grid-cols-2 gap-2">
             <button
+              @click="openModalSell(item)"
               class="bg-blue-500 text-white px-3 p-1 rounded-md active:scale-95 duration-200 text-sm"
             >
               Sotish
@@ -119,20 +120,208 @@
       </div>
     </div>
   </div>
+  <!-- Begin Edit Product modal -->
+  <!-- Main modal -->
+  <div
+    tabindex="-1"
+    aria-hidden="true"
+    :class="
+      sellModal
+        ? 'flex overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full '
+        : 'hidden '
+    "
+    class=""
+  >
+    <div
+      class="relative flex items-center justify-center p-4 w-full max-w-full max-h-full"
+    >
+      <!-- Modal content -->
+      <div
+        class="relative bg-white w-full md:w-1/2 rounded-lg shadow-sm dark:bg-gray-700 border"
+      >
+        <!-- Modal header -->
+        <div
+          class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600 border-gray-200"
+        >
+          <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
+            Mahsulot Sotish
+          </h3>
+          <button
+            @click="closeModeal()"
+            type="button"
+            class="end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+            data-modal-hide="authentication-modal"
+          >
+            <svg
+              class="w-3 h-3"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 14 14"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
+              />
+            </svg>
+            <span class="sr-only">Close modal</span>
+          </button>
+        </div>
+        <!-- Modal body -->
+        <div class="w-full px-4 pt-4 flex justify-between">
+          <span class="flex flex-col gap-1 font-bold"
+            ><h1
+              class="block text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Mahsulot nomi:
+            </h1>
+            {{ sellProduct.name }}</span
+          >
+          <span class="flex flex-col gap-1 font-bold"
+            ><h1
+              class="block text-sm font-medium text-gray-900 dark:text-white"
+            >
+              Mahsulot hajmi:
+            </h1>
+            {{ sellProductSize }} Kg</span
+          >
+        </div>
+        <div class="w-full p-4 md:p-5">
+          <form class="space-y-4 w-full" action="#">
+            <div class="flex gap-4">
+              <div class="w-1/2">
+                <label
+                  for="numberheigh"
+                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >Mahsulot Hajmi(kg)</label
+                >
+                <input
+                  type="number"
+                  name="numberheigh"
+                  v-model="sellProduct.size"
+                  :class="
+                    sellProduct.size > sellProductSize
+                      ? 'border-red-500 focus:border-red-500 focus:ring-red-500'
+                      : ''
+                  "
+                  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                  placeholder="500"
+                  required
+                />
+              </div>
+              <div class="w-1/2">
+                <label
+                  for="price"
+                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >Sotish Narxi(UZS)</label
+                >
+                <input
+                  type="number"
+                  name="price"
+                  v-model="sellProduct.price"
+                  placeholder="5000"
+                  :class="
+                    sellProduct.price < sellProduct.buyyingPrice
+                      ? 'border-red-500 focus:border-red-500 focus:ring-red-500'
+                      : ''
+                  "
+                  class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                  required
+                />
+              </div>
+            </div>
+            <div
+              class="flex justify-between border-t pt-2 rounded-t dark:border-gray-600 border-gray-200"
+            >
+              <span
+                ><label
+                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >Sotiladigan mahsulot</label
+                >
+                <h1 class="font-bold">
+                  {{ sellProduct.size ? sellProduct.size : 0 }} kg
+                </h1></span
+              >
+              <span
+                ><label
+                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >Tushkan Summa</label
+                >
+                <h1 class="font-bold">
+                  {{
+                    formatCurrency(sellProduct.price ? sellProduct.price * sellProduct.size : 0)
+                  }}
+                </h1></span
+              >
+              <span
+                ><label
+                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >Sof Foyda</label
+                >
+                <h1 class="font-bold">
+                  {{
+                    formatCurrency(sellProduct.price
+                      ? sellProduct.price * sellProduct.size -
+                        sellProduct.buyyingPrice * sellProduct.size
+                      : 0)
+                  }}
+                </h1></span
+              >
+            </div>
+            <button
+              type="submit"
+              class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Sotish
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <!-- End Edit Product modal -->
 </template>
 <script setup>
 import axios from "axios";
-import { onMounted, ref } from "vue";
+import { onMounted, ref, reactive } from "vue";
 import formatCurrency from "../../utils/PriceFormatter";
 
+const sellProduct = reactive({
+  name: null,
+  size: null,
+  price: null,
+  buyyingPrice: null,
+});
+const sellProductSize = ref();
+const sellModal = ref(false);
 const data = ref();
 const getProduct = async () => {
   try {
     const res = await axios.get("/api/product");
     data.value = res.data;
+    console.log(res.data);
   } catch (error) {
     console.log(error);
   }
+};
+const openModalSell = async (item) => {
+  sellModal.value = true;
+  sellProduct.price = item.price;
+  sellProduct.buyyingPrice = item.buyyingPrice;
+  sellProduct.name = item.name;
+  sellProductSize.value = item.size;
+  console.log(sellProduct.buyyingPrice);
+};
+const closeModeal = () => {
+  sellModal.value = false;
+  sellProduct.name = null;
+  sellProduct.size = null;
+  sellProduct.price = null;
+  sellProductSize.value = null;
 };
 
 const deleteProduct = async (id) => {
